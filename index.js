42fd4d76c03900776bfd0ff9eda64e5f2acfa32a
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2024-01-14 09:48:23 +0100

    Implement interactive drum kit functionality

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var buttons = document.querySelectorAll(".drum");
 
+function playSound(sound) {
+  new Audio(`./sounds/${sound}.mp3`).play();
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    var soundType = button.getAttribute("sound-type");
+    playSound(soundType);
+  });
+});
